64001a26b1dc2e7ad67f13ff5bd61186a56249bf
Peter Schröder <nofail@peter-schroders-macbook.local>
2010-01-31 17:31:28 +0100

    better usage example

--- examples/rebuil_examples.rb
+++ examples/rebuil_examples.rb
@@ -1,13 +1,9 @@
 require 'example_helper'
 
-puts "cool" if rebuil("hello world") do 
-	many
-end.line_end =~ "hello world with rebuil"
-
 match = rebuil("hello world") do
   many
 	group("rebuil")
 end.match('hello world with rebuil')
 
-puts match[1]
+puts "#{match[1]} is cool"
 
